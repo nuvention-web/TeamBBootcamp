83626a2b3d916debbb4a6f4adb81e732e3dad81c
Eli <elicohen2018@u.northwestern.edu>
2018-01-17 23:54:27 -0600

    autofilling week and milestons

--- src/app/statusreport/statusreport.component.ts
+++ src/app/statusreport/statusreport.component.ts
@@ -10,18 +10,25 @@ export class StatusreportComponent implements OnInit {
 
 	openissues:any = null
 	closedissues:any = null
-	repositoryowner = "nuvention-web"
-	repositoryname = "teambbootcamp"
+	repositoryowner: string = "nuvention-web"
+	repositoryname: string = "teambbootcamp"
+  dateString: string = ""
+  currentMilestone: string = ""
+  nextMilestone: string = ""
+
+  SECONDS_IN_DAY: any = 60000*60*24
 
 
   constructor(private issueService: IssueService) { }
 
   ngOnInit() {
+    this.dateString = this.getFormattedDate()
   }
 
   getStatusReport(): void {
   	var repositoryUrl = "https://api.github.com/repos/" + this.repositoryowner + "/" + this.repositoryname + "/issues?state=all"
   	this.getIssues(repositoryUrl);
+    this.updateMilestones();
   }
 
   downloadPage(): void {
@@ -36,7 +43,6 @@ export class StatusreportComponent implements OnInit {
     document.body.appendChild(a);
     a.innerHTML = "something random - nobody will see this, it doesn't matter what you put here";
     a.click();
-
   }
 
   filterIssues(issues): void {
@@ -49,5 +55,30 @@ export class StatusreportComponent implements OnInit {
 	  this.issueService.getIssues(repositoryUrl)
 	    .subscribe(issues => this.filterIssues(issues));
 	}
+
+  updateMilestones(): void {
+    this.issueService.getIssues("https://api.github.com/repos/nuvention-web/TeamBBootcamp/milestones")
+      .subscribe(milestones => this.setMilestones(milestones));
+  }
+
+  setMilestones(milestones): void {
+    this.nextMilestone = milestones[0].title;
+    this.currentMilestone = milestones[1].title;
+  }
+
+  getFormattedDate(dateObj = null): string {
+    // defaults to current date but accepts custom date if provided as parameter
+    var dateObj = dateObj == null ? new Date() : dateObj;
+    var month = dateObj.getUTCMonth() + 1;
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+
+    return month + "/" + day + "/" + year;
+  }
+
+  getDateSomeDaysAgo(days): string {
+    var newDateObj = new Date(new Date().getTime()-this.SECONDS_IN_DAY*days);
+    return this.getFormattedDate(newDateObj);
+  }
 
 }
